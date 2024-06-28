e620c85d39216a12e895eff454a6162b8d8c9971
Tuni-Soft <amxmodx.unloco@gmail.com>
2024-06-28 14:07:56 +0100

    Repro

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	console.log('start');
+</script>
+
+/** eslint-disable no-console */ /** eslint-disable no-console */
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+// eslint-disable-next-line no-console
+<div onclick={() => console.log('ok')}></div>
