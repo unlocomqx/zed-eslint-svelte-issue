3e63a7e18dd7e9c6d1dc975dd39cb6715caf7de5
Tuni-Soft <amxmodx.unloco@gmail.com>
2024-07-15 20:13:54 +0100

    update eslint to latest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	console.log('start');
 </script>
 
-/** eslint-disable no-console */ /** eslint-disable no-console */
+/** eslint-disable no-console */
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
